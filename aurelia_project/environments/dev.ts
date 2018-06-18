fe8a7a17ed002d126259de8b5323eb661dfe67b1
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-18 20:48:18 -0300

    auth-service

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -1,5 +1,9 @@
 export default {
   debug: true,
   testing: true,
-  apiKey: '860f692511fe86e7f3845e7bb0e8edb0'
+  apiKey: 'd25d7a6ef37e1f8d1d30d96be59c6a3e',
+  auth0: {
+    clientId: '4Hf5632gZacg0vGmLq1zzzWCjH9J8tUA',
+    domain: 'bk-tmp.auth0.com'
+  }
 };
