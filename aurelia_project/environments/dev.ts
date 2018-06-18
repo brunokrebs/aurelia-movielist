815784b2e56cfecb5c99689c335035efdc1a355c
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-18 19:39:30 -0300

    api wrapper

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -1,4 +1,5 @@
 export default {
   debug: true,
-  testing: true
+  testing: true,
+  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjBmNjkyNTExZmU4NmU3ZjM4NDVlN2JiMGU4ZWRiMCIsInN1YiI6IjViMjgyZTMyYzNhMzY4NjRiNTAwMmZmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ'
 };
