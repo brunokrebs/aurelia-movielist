5e6f588cebf0a9f573ac7d6cc797dc6db2a1c81e
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-18 19:51:16 -0300

    movie component and updating jest-cli

---
+++ src/components/movie.ts
@@ -0,0 +1,15 @@
+import { bindable } from 'aurelia-framework';
+import { Movie as MovieModel } from 'moviedb-promise';
+
+export class Movie {
+  @bindable model: MovieModel;
+  isHovering = false;
+
+  mouseOver() {
+    this.isHovering = true;
+  }
+
+  mouseOut() {
+    this.isHovering = false;
+  }
+}
